200e237f04e3899df0163cd6908a85edb40c1bd4
carlos <cgarciapieto@gmail.com>
2019-02-25 12:31:34 -0600

    first push

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("", views.index, name=""),
+    path("helloAccounts/", views.index, name="helloAccounts"),
+    path("addFive/<int:accountID>", views.addFive, name ="addFive")
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test URL")
+    allAccounts = Account.objects.all()
+    context = {
+        "allAccounts": allAccounts
+    }
+    return render(request, 'accountApp/index.html', context),
+# render hello plus account name
+def helloAccount(request):
+
+    return HttpResponse("hello " + request.POST['accountName'])
+#  adds five to the account using the ID
+def addFive(request, accountID):
+    individualAccount = get_object_or_404(Account, pk = accountID)
+    individualAccount.checking += 5
+    individualAccount.save()
+
+    return redirect("index")
+
+
